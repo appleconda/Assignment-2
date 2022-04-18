93ba88174786fbfbfb82fe6b90b0c95740115646
Abdullah Chaudhry <appleconda@gmail.com>
2022-04-18 20:13:20 +0500

    q3 completed

--- q3.cpp
+++ q3.cpp
@@ -1,90 +1,83 @@
-#include  <fcntl.h>                              //
-#include  <stdio.h>                              //
-#include  <stdlib.h>                             //
-#include  <string.h>                             //
-#include  <sys/types.h>                          //
-#include  <sys/wait.h>                           //
-#include  <sys/stat.h>                           //
-#include  <termios.h>                            //
-#include  <unistd.h> 
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <iostream> 
+#include <random> 
+#include <string>
+#include <vector> 
 
-using namespace std;
+#define READ_END 0
+#define WRITE_END 1
 
-class employee
+using namespace std; 
+
+//----------c++ 11 Standards random function 
+int randGen(int& min, int& max)
 {
-    int start_time;
-    int end_time;
-
-    employee()
-    {
-        start_time = 9;
-        end_time = 5;
-    }
-};
-
-
+    std::random_device device; 
+    std::mt19937 generator(device()); 
+    std::uniform_int_distribution<int> distribution(min, max); 
+    return distribution(generator); 
+}
+//-----------------------------------------
 
 int main()
 {
-    pid_t pids[10];
-    int i;
-    int n = 10;
     
-
-    int f1 = mkfifo("p", 0666);                                         //Making named pipe
-    
-    if (f1 < 0)
-        std::cerr << "Pipe not created";
-
-    char str[256] = "9-6";   
-    char read_char[256] = "";                                            
-    /* Start children. */
-    for (i = 0; i < 10; ++i) {
-        if ((pids[i] = fork()) < 0) {
-            perror("fork");
-            abort();
-        }
-        else if (pids[i] == 0) {
-            int fifo_write = open("p", O_WRONLY);
-            if (fifo_write < 0)
-            {
-                std::cerr << "Pipe could not be created";
-                return 0;
-            }
-            else
-            {
-               
-                write(fifo_write, str, sizeof(str));
-                close(fifo_write);
-                exit(0); 
-            }
-            
-            
-        }
-
-    }
-
-    
-    
-
-    /* Wait for children to exit. */
+    int endTime = 0; 
    
-    int count(0); 
-    int fifo_read = open("p", O_RDONLY);
-    char str1[256] = ""; 
-    if (fifo_read < 0)
+    int min = 1; 
+    int max = 6; 
+
+    int fd[10][2]; //fd represeting ten pipes
+
+
+    for (auto i = 0; i < 10; i++)
     {
-        std::cerr << "Pipe could not be created";
+        if (pipe(fd[i]) == -1)
+        {
+            fprintf(stderr, "Pipe not created");
+
+        }
     }
-    else
+
+    for(int i=0;i<10;i++) // loop will run n times (n=5)
     {
+        if(fork() == 0)
+        {
+            int val;    
+            val = randGen(min, max);
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
             
-        read(fifo_read, str1, sizeof(str));
-        cout << str1 << endl;
-        count++; 
-        close(fifo_read);
+            cout << "The Val (sending to pipe) = " << val << endl; 
+            
+            close(fd[i][READ_END]);
+            
+            write(fd[i][WRITE_END], &val, sizeof(val));
+            
+            close(fd[i][WRITE_END]);
+            exit(0);
+        }
+        else
+        {
+            int readValue = 0; 
+            wait(NULL);
+            
+            
+            close(fd[i][WRITE_END]);
+            
+            read(fd[i][READ_END], &readValue, sizeof(readValue));
+            cout << "The read vlaue is " << readValue << endl; 
+
+            
+            close(fd[i][READ_END]);
+        }
     }
-    cout << "the count is " << count << endl; 
-    unlink("p");
+  
+  
+      
 }
